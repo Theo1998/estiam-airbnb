f9f93bd90fb2d0d4652f9fa96e13b4a3fa5c0458
TANCHOUX Théo <tanchoux71@gmail.com>
2020-09-28 11:33:05 +0200

    server configuration

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  uri: 'mongodb+srv://admin:ESTIAM-admin-airbnb%23%212020@cluster0.jijvf.gcp.mongodb.net/airbnb?retryWrites=true&w=majority',
+};
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
+const db = require('../config/database');
+
+const router = express.Router();
+
+const port = process.env.PORT || 8000;
+
+app.use(morgan('combined'));
+
+app.use(cors());
+mongoose.Promise = global.Promise;
+mongoose.connect(db.uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+router.get('/', (req, res) => {
+  res.send('Welcome to our Airbnb API !');
+});
+
+router.use((req, res) => {
+  res.send('<h1> Page not found </h1>');
+});
+
+app.use('/api', router);
+
+app.listen(port, () => console.log(`[🚧 server is running on ${port}]`));
